ab24ff6193b3ede084d87f5035b7665fdd26ea7f
havardsollie <79274753+havardsollie@users.noreply.github.com>
2022-03-07 14:28:49 +0100

    localstorage

--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
+import { getExistingFavorites } from "./utilities/getExistingFavorites.js";
+
 const url = "http://localhost:1337/articles";
 const container = document.querySelector(".container");
+const favorites = getExistingFavorites();
 
-export async function createList() {
+async function createList() {
   const response = await fetch(url);
   const data = await response.json();
   
-  for (let i = 0; i < data.length; i++) {
-    let title = data[i].title;
-    let summary = data[i].summary;
-    let author = data[i].author;
+  data.forEach((product) => {
+    let cssClass = "far";
+
+    const doesArticleExist = favorites.find(function (fav) {
+      return parseInt(fav.id) === data.id;
+    })
+
+    if (doesArticleExist) {
+      cssClass = "fa";
+    }
+ 
+    let title = product.title;
+    let summary = product.summary;
+    let author = product.author;
     container.innerHTML += `<div class="products">
                               <h2>${title}</h1>
                               <p>${summary}</p>
                               <p>${author}</p>
-                              <i class="far fa-heart" data-id"${data[i].id}" data-name="${title}"></i>
+                              <i class="far fa-heart" data-id"${product.id}" data-name="${title}"></i>
                             </div>`
-  }
-  const iconToClick = document.querySelectorAll(".products i");
-  console.log(iconToClick);
 
-  iconToClick.forEach((icon) => {
-      icon.addEventListener("click", addToList);
-  });
+    const iconToClick = document.querySelectorAll(".products i");
+    console.log(iconToClick);
+
+    iconToClick.forEach((icon) => {
+        icon.addEventListener("click", addToList);
+    });
+})
 }
 
 createList();
@@ -35,28 +49,18 @@ function addToList() {
 
   const currentFavorites = getExistingFavorites();
 
-  const doesArticleExist = currentFavorites.find(function(fav) {
+  const ifArticleExist = currentFavorites.find(function(fav) {
     return fav.id === id;
   });
   
-  if (doesArticleExist === undefined) {
+  if (ifArticleExist === undefined) {
     const article = { id: id, title: name };
     currentFavorites.push(article);
     saveToFavorites(currentFavorites);
   }
   else {
-    const newFavorites = currentFavorites.filter(fav => fav.id !== id);
+    const newFavorites = currentFavorites.filter((fav) => fav.id !== id);
     saveToFavorites(newFavorites);
-  }
-}
-
-function getExistingFavorites() {
-  const favorites = localStorage.getItem("favorites");
-
-  if(favorites === null) {
-    return [];
-  } else {
-    return JSON.parse(favorites);
   }
 }
 
---
+++ js/utilities/getExistingFavorites.js
@@ -0,0 +1,9 @@
+export function getExistingFavorites() {
+  const favorites = localStorage.getItem("favorites");
+
+  if(favorites === null) {
+    return [];
+  } else {
+    return JSON.parse(favorites);
+  }
+}
